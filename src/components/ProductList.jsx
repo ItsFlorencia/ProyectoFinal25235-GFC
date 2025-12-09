178f54c7c25443610270b6835c8e2469fb9fa5bf
G.Florencia Castillo <florenciac09@gmail.com>
2025-12-09 20:31:05 -0300

    Modificacion PL

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,19 +1,22 @@
-import React, { useEffect, useState } from 'react';
-import { Row, Col, Modal, Button } from 'react-bootstrap';
+import React, { useEffect, useState, useContext } from 'react';
+import { Row, Col, Form, Modal, Button } from 'react-bootstrap';
 import ProductCard from './ProductCard';
+import { CartContext } from './CartContext';
 
 const ProductList = ({ category = "women's clothing" }) => 
     {
+  const ProductList = ({ category = null }) => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
+  const { agregarAlCarrito } = useContext(CartContext);
 
 
     useEffect(() => 
     {
-    let url = 'https://fakestoreapi.com/products';
+    let url = 'https://693360d4e5a9e342d2729e37.mockapi.io/Products';
     if (category) 
     {
-      url = `https://fakestoreapi.com/products/category/${category}`;
+      url = `https://69377263f8dc350aff344105.mockapi.io//${category}`;
     }
 
       fetch(url)
@@ -27,30 +30,23 @@ const ProductList = ({ category = "women's clothing" }) =>
         setLoading(false);
       });
   }, [category]);
-
-
-    const handleAgregarAlCarrito = (product) => {
-    
-    alert(`Se ha agregado al carrito: ${product.title} `);
-   
-  };
-
-
-
+  
   if (loading) {
     return <div>Loading...</div>;
   }  
 
   
   return (
+    <>
     <Row>
-      {products.map((product) => (
+       {filteredProducts.map((product) => (
         <Col md={4} key={product.id} className="mb-4">
-          <ProductCard product={product} agregarAlCarrito={handleAgregarAlCarrito} />
+          <ProductCard product={product} agregarAlCarrito={agregarAlCarrito} />
         </Col>
       ))}
     </Row>
+     </>
   );
 };
-
+};
 export default ProductList;
